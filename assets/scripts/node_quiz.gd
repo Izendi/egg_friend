2fd970b80dcd29d5b1d001f50d7e19fd045c603f
Izendi <cjwinters1404@gmail.com>
2025-11-05 14:37:32 +0900

    adding in questions

--- assets/scripts/node_quiz.gd
+++ assets/scripts/node_quiz.gd
@@ -6,6 +6,11 @@ var questionData: Quiz_Question_Data
 func _ready():
 	questionData = Quiz_Question_Data.new()
 	questionData.generate_new_question_data()
+	
+	
+
+func loadQuestions(path: String) -> Array:
+	load
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
